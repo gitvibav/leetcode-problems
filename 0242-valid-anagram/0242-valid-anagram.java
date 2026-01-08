76f15d3ef8daca5299c261b7240ae62ad0ae58eb
Vibav Mahendran <128117178+gitvibav@users.noreply.github.com>
2026-01-08 11:27:51 +0530

    Time: 17 ms (17.71%), Space: 45.2 MB (51.7%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,28 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char first_str[] = new char[26];
-        char second_str[] = new char[26];
+        int[] freq_s = new int[26];
+        int[] freq_t = new int[26];
 
         for(int i=0;i<s.length();i++){
-            first_str[s.charAt(i) - 'a']++;
+            freq_s[s.charAt(i) - 'a']++;
         }
 
         for(int i=0;i<t.length();i++){
-            second_str[t.charAt(i) - 'a']++;
+            freq_t[t.charAt(i) - 'a']++;
+        }
+
+        for(int i : freq_s) {
+            System.out.print(i+ " ");
+        }
+
+        System.out.println();
+
+        for(int i : freq_t) {
+            System.out.print(i+ " ");
         }
 
         for(int i=0;i<26;i++){
-            if(first_str[i] != second_str[i]){
+            if (freq_s[i] != freq_t[i]) {
                 return false;
             }
         }
