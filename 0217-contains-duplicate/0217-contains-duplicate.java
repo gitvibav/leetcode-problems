582706de5777a9dc43c9b4fb6b0013a2c55c9429
Vibav Mahendran <128117178+gitvibav@users.noreply.github.com>
2026-01-08 11:21:00 +0530

    Time: 16 ms (45.67%), Space: 97.9 MB (10.41%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
+        Map<Integer,Integer> hm = new HashMap<>();
 
         for(int i=0;i<nums.length;i++){
 
@@ -8,7 +8,7 @@ class Solution {
                 return true;
             }
 
-            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+            hm.put(nums[i], i);
         }
 
         return false;
