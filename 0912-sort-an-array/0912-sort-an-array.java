72be0ce1687bc8cb195ac4163106e0ee1cc70498
Vibav Mahendran <128117178+gitvibav@users.noreply.github.com>
2026-01-09 17:44:22 +0530

    Time: 65 ms (22.37%), Space: 74.9 MB (96.28%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,26 +1,26 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        mergeSort(nums,0,nums.length-1);
+        mergeSort(nums, 0, nums.length-1);
         return nums;
     }
 
-    public void mergeSort(int nums[],int start, int end){
+    public void mergeSort(int[] nums, int start, int end){
         if(start == end){
             return ;
         }
         int mid = (start+end)/2;
-        mergeSort(nums,start,mid);
-        mergeSort(nums,mid+1,end);
-        merge(nums,start,mid,end);
+        mergeSort(nums, start, mid);
+        mergeSort(nums, mid+1, end);
+        merge(nums, start, mid, end);
     }
 
-    public void merge(int nums[],int start, int mid, int end){
+    public void merge(int[] nums, int start, int mid, int end) {
         int left = start;
         int right = mid+1;
+        ArrayList<Integer> temp = new ArrayList<>();
 
-        List<Integer> temp = new ArrayList<>();
-        while(left <= mid && right <= end){
-            if(nums[left] <= nums[right]){
+        while(left <= mid && right <= end) {
+            if(nums[left] <= nums[right]) {
                 temp.add(nums[left]);
                 left++;
             }
@@ -30,18 +30,18 @@ class Solution {
             }
         }
 
-        while(left <= mid){
+        while(left <= mid) {
             temp.add(nums[left]);
             left++;
-        }
+        } 
 
-        while(right <= end){
+        while(right <= end) {
             temp.add(nums[right]);
             right++;
         }
 
         for(int i=start;i<=end;i++){
             nums[i] = temp.get(i-start);
-        }
+        }       
     }
 }
