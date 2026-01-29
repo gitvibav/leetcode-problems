028a67fa8967ea4c1cc8ba42b070972452259135
Vibav Mahendran <128117178+gitvibav@users.noreply.github.com>
2026-01-29 08:32:44 +0530

    Time: 4 ms (36.23%), Space: 6 MB (40.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,18 +1,13 @@
 func twoSum(nums []int, target int) []int {
-    hash := make(map[int]int)
+    idx := make(map[int]int)
 
-    for i,v := range nums {
-        elem, ok := hash[target - v];
-        
-        if ok {
-            return []int{i, elem}
-        }
-
-        if _, ok := hash[v]; !ok {
-            hash[v] = i;
+    for k,v := range nums {
+        if _, ok := idx[target - v]; !ok {
+            idx[v] = k
+        } else {
+            return []int{k, idx[target-v]}
         }
     }
 
-    return []int{0,0}
-
+    return []int{0,0};
 }
